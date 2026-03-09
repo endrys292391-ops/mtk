8ad9431a98a42489d27d7916467ff1f517311d49
endrys292391-ops <endrys292391@gmail.com>
2026-03-09 22:35:46 +0300

    config.py

--- config.py
+++ config.py
@@ -3,14 +3,17 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
-# Токен бота (вставь свой)
-BOT_TOKEN = "8241703921:AAEeGhfVfySLxE0rfI9PA6q9SeYdKsn2nxA"
+# Токен бота
+BOT_TOKEN = "7618653140:AAFLWh4VFiKb3Ig6c0tQNyEi0byxdhC0Usk"
+
+# ТВОЙ ID (админ)
+ADMIN_ID = 8591272970
 
 # База данных
 DATABASE_PATH = BASE_DIR / "bot_database.db"
 
-# Логи
+# Лог файл
 LOG_FILE = BASE_DIR / "bot.log"
 
 # Настройки парсинга
-USE_SELENIUM = False  # Отключаем Selenium если не нужен
+USE_SELENIUM = False
